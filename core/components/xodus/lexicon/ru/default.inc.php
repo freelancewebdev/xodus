f3c232feafba3c9402005664a34c23191d12b7db
Joe Molloy <molloyjoe@gmail.com>
2012-11-12 20:27:39 +0000

    Addition of Russian

---
+++ core/components/xodus/lexicon/ru/default.inc.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Xodus
+ *
+ * Copyright 2012 by Joe Molloy <info@hyper-typer.com>
+ *
+ * This file is part of Xodus, a simple component MODx Revolution which allows users from a selected user group to be exported to CSV or MS Excel format.
+ *
+ * Xouds is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Xodus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Xodus; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package xodus
+ */
+ 
+$_lang['xodus'] = 'Xodus';
+$_lang['xodus.desc'] = 'Экспорт пользователей MODX';
+$_lang['xodus.intro'] = 'Выбор группы для экспорта и форматирования файла..';
+$_lang['xodus.group'] = 'Группа';
+$_lang['xodus.format'] = 'Форматирование файла';
+$_lang['xodus.error_group'] = 'Выберите группу из которой следует экспортировать пользователей';
+$_lang['xodus.error_file_format'] = 'Для продолжения выберите формат файла';
+$_lang['xodus.export'] = 'Экспорт пользователей';
+$_lang['xodus.default_back'] = 'Отмена';
+$_lang['xodus.export_users'] = 'Пользователи удачно экспортированны. Ваша закачка начинается...';
+$_lang['xodus.export_no_users'] = 'В настоящее время в данной группе нет пользователей для экспорта';
+$_lang['xodus.full_name'] = 'Полное имя';
+$_lang['xodus.email'] = 'Электронный адрес';
+$_lang['xodus.password'] = 'Пароль';
+$_lang['xodus.phone'] = 'Стационарный телефон';
+$_lang['xodus.mobile'] = 'Моб. телефон';
+$_lang['xodus.fax'] = 'Факс';
+$_lang['xodus.address'] = 'Адрес';
+$_lang['xodus.city'] = 'Город';
+$_lang['xodus.state'] = 'Штат';
+$_lang['xodus.zip'] = 'Почтовый код';
+$_lang['xodus.country'] = 'Страна';
+$_lang['xodus.dob'] = 'Дата рождения';
+$_lang['xodus.gender'] = 'Пол';
+$_lang['xodus.photo'] = 'Фото';
+$_lang['xodus.website'] = 'Сайт';
+$_lang['xodus.comment'] = 'Комментарии';
+$_lang['xodus.extended_fields'] = 'Дополнительная информация';
+$_lang['xodus.blocked'] = 'Заблокировано';
+$_lang['xodus.blocked_after'] = 'Заблокированo с';
+$_lang['xodus.blocked_until'] = 'Заблокировано до';
+$_lang['xodus.last_login'] = 'Последний заход';
+$_lang['xodus.login_count'] = 'Количество заходов ';
+$_lang['xodus.failed_login_count'] = 'Количество неудавшихся заходов';
+$_lang['xodus.session_id'] = 'ID сессии';
+$_lang['xodus.error'] = 'Ошибка';
+$_lang['xodus.error_text'] = 'Произошла ошибка';
+$_lang['xodus.return_link_text'] = 'Нажмите для возврата ';
+$_lang['xodus.fix_write'] = 'Пожалуйста, убедитесь, что  PHP может быть написан  в следующей директории';
