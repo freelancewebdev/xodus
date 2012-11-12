9d27472ff908320443d5ba40385eabe1a6f82a0e
Joe Molloy <molloyjoe@gmail.com>
2012-11-12 19:09:29 +0000

    Addition of Spanish Lexicon

---
+++ core/components/xodus/lexicon/es/default.inc.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Xodus
+ *
+ * Copyright 2012 by Joe Molloy <info@hyper-typer.com>
+ *
+ * This file is part of Xodus, a simple component MODx Revolution which allows users from a selected user group to be exported to CSV or MS Excel format.
+ *
+ * Xouds is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Xodus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Xodus; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package xodus
+ */
+ 
+$_lang['xodus'] = 'Xodus';
+$_lang['xodus.desc'] = 'MODX Exportar Usuarios';
+$_lang['xodus.intro'] = 'Elija un grupo a exportar y el formato de archivo que requiere.';
+$_lang['xodus.group'] = 'Grupo';
+$_lang['xodus.format'] = 'Formato de Archivo';
+$_lang['xodus.error_group'] = 'Debe elegir un grupo del cual exportar los usuario';
+$_lang['xodus.error_file_format'] = 'Debe elegir un formato de archivo para continuar';
+$_lang['xodus.export'] = 'Exportar';
+$_lang['xodus.default_back'] = 'Cancelar';
+$_lang['xodus.export_users'] = 'usuarios exportados exitosamente. Su descarga está comenzando...';
+$_lang['xodus.export_no_users'] = 'Actualmente en este grupo no hay usuarios para exportar';
+$_lang['xodus.full_name'] = 'Nombre Completo';
+$_lang['xodus.email'] = 'Email';
+$_lang['xodus.password'] = 'Contraseña';
+$_lang['xodus.phone'] = 'Teléfono';
+$_lang['xodus.mobile'] = 'Celular/Móvil';
+$_lang['xodus.fax'] = 'Fax';
+$_lang['xodus.address'] = 'Dirección';
+$_lang['xodus.city'] = 'Ciudad';
+$_lang['xodus.state'] = 'Estado/Provincia';
+$_lang['xodus.zip'] = 'Código Postal';
+$_lang['xodus.country'] = 'Pais';
+$_lang['xodus.dob'] = 'Fecha de Nacimiento';
+$_lang['xodus.gender'] = 'Género';
+$_lang['xodus.photo'] = 'Foto';
+$_lang['xodus.website'] = 'Sitio Web';
+$_lang['xodus.comment'] = 'Comentarios';
+$_lang['xodus.extended_fields'] = 'Campos Extendidos';
+$_lang['xodus.blocked'] = 'Bloqueado';
+$_lang['xodus.blocked_after'] = 'BBloqueado Después';
+$_lang['xodus.blocked_until'] = 'Bloqueado Hasta';
+$_lang['xodus.last_login'] = 'Último Ingreso';
+$_lang['xodus.login_count'] = 'Cantidad de ingresos';
+$_lang['xodus.failed_login_count'] = 'Cantidad de ingresos fallidos';
+$_lang['xodus.session_id'] = 'Identificador de Sesión';
+$_lang['xodus.error'] = 'Error';
+$_lang['xodus.error_text'] = 'Ha ocurrido un error';
+$_lang['xodus.return_link_text'] = 'Presione aquí para volver';
+$_lang['xodus.fix_write'] = 'Por favor asegúrese que PHP puede escribir en el siguiente directorio';
